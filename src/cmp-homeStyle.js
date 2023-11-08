bd81dbdea76dfed67f4d085c80c3946d6298e908
afsil6236@misena.edu.co <afsil6236@misena.edu.co>
2023-11-08 08:17:05 -1200

    Evidencianumero1

---
+++ src/cmp-homeStyle.js
@@ -0,0 +1,66 @@
+import { css } from "lit-element";
+
+export default css `
+#cajas{
+    height: 70px;
+    font-size: 40px;
+    font-weight: bold;
+    background-color: blue; /* Cambiar el color a azul */
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px;
+    color: white;
+}
+#btnFiltrar{
+    width: 700px;
+}
+#pt{
+    padding-top: 210px;
+}
+#cajas2{
+    width: 200px;
+    height: 70px;
+    font-size: 15px;
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
+    background-color: green; /* Cambiar el color a verde */
+    color: white;
+}
+#cuadro{
+    width: 55rem;
+    height: 29rem;
+    border-radius: 1rem;
+    border-color: black;
+}
+
+/* Estilo para el modal */
+.modal {
+    display: none;
+    position: fixed;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%); /* Centrar horizontalmente */
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.7);
+    z-index: 9999;
+    justify-content: center;
+    align-items: center;
+}
+
+.modal-content {
+    background: white;
+    width: 60%;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+}
+
+.cut-call-button {
+    background: orange; /* Cambiar el color a naranja */
+    color: white;
+    border: none;
+    border-radius: 5px;
+    padding: 10px 20px;
+    cursor: pointer;
+}
+`
---
+++ src/cmp-loginStyle.js
@@ -0,0 +1,22 @@
+import { css } from "lit-element";
+
+export default css `
+#form{
+    background: linear-gradient(to right, #33cc33, #66ff66); /* Cambiar el gradiente a tonos de verde */
+    padding: 98px;
+    border-radius: 20px;
+}
+#form2{
+    background: linear-gradient(to right, #ff9900, #ffcc66); /* Mantener el gradiente de naranja */
+    padding: 98px;
+    border-radius: 20px;
+    right: 100px;
+    border: 2px solid #ff3333; /* Mantener el color del borde a rojo */
+}
+#container-username{
+    background-color: #33cc33; /* Cambiar el color de fondo a verde para que combine con el formulario */
+}
+#container-password{
+    background-color: #33cc33; /* Cambiar el color de fondo a verde para que combine con el formulario */
+}
+`
